ada96bcc4fe07a2954e4166728dca5b7a1a9b66f
josephgcedeno <josephgcedeno@gmail.com>
2024-11-08 11:47:32 +0800

    feat: provide sample cubit events

--- lib/core/module/home/application/service/cubit/home_cubit.dart
+++ lib/core/module/home/application/service/cubit/home_cubit.dart
@@ -1,27 +1,112 @@
-import 'package:flirt/core/application/service/cubit/quote_dto.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flirt/core/infrastructures/repository/local_repository.dart';
+import 'package:flirt/core/infrastructures/repository/remote_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
-  HomeCubit()
-      : super(
-          HomeState(
-            data: QuoteStateDTO(
-              authors: <String>[],
-              quotes: <QuoteResponseDTO>[],
-            ),
-          ),
-        );
+  HomeCubit({
+    required this.localRepository,
+    required this.remoteRepository,
+  }) : super(HomeState());
+  final LocalRepository localRepository;
+  final RemoteRepository remoteRepository;
 
   /// This method is used to store a new quote and its author in the state.
-  /// It appends the quote to the list of quotes and the author to the list of authors.
-  ///
-  /// [quote] Single response from the quote
-  Future<void> storeState(QuoteResponseDTO quote) async {
-    /// This approach is used to demonstrate persistent data storing that you might use on cases where you need to hold data for a series of screens in a module.
-    /// For example: If you separate a lengthy registration form in to 5 separate screens, you would need to hold that data until the last step.
-    state.data.quotes.add(quote);
-    state.data.authors.add(quote.author);
+  // /// It appends the quote to the list of quotes and the author to the list of authors.
+  // ///
+  // /// [quote] Single response from the quote
+  Future<void> saveToLocal(List<Item> response) async {
+    if (localRepository.database == null) {
+      await localRepository.open();
+    }
+
+    for (int i = 0; i < response.length; i++) {
+      await localRepository.insert(response[i]);
+    }
+  }
+
+  Future<bool> get hasConnectivity async =>
+      (await Connectivity().checkConnectivity()).first !=
+      ConnectivityResult.none;
+
+  Future<void> getAllData() async {
+    emit(FetchItemsLoading());
+    try {
+      if (localRepository.database == null) {
+        await localRepository.open();
+      }
+
+      if (await hasConnectivity) {
+        final List<Item> res = await remoteRepository.getItems();
+        await saveToLocal(res);
+      }
+
+      final List<Item> getLocalItems = await localRepository.getItems();
+
+      emit(FetchItemsSuccess(items: getLocalItems));
+    } catch (_) {
+      emit(FetchItemsFailed());
+    }
+  }
+
+  Future<void> addData(Item item) async {
+    emit(AddItemsLoading());
+    try {
+      if (localRepository.database == null) {
+        await localRepository.open();
+      }
+
+      if (await hasConnectivity) {
+        item.isSync = true;
+        await remoteRepository.addData(item);
+      }
+
+      await localRepository.insert(item);
+
+      emit(AddItemsSuccess(item: item));
+    } catch (_) {
+      emit(AddItemsFailed());
+    }
+  }
+
+  Future<List<Item>> getLocalItems() async {
+    if (localRepository.database == null) {
+      await localRepository.open();
+    }
+    final List<Item> res = await localRepository.getItems();
+
+    return res;
+  }
+
+  Future<void> syncDataWhenOnline() async {
+    if (!await hasConnectivity) {
+      emit(FetchSyncDataFailed());
+      return;
+    }
+
+    final List<Item> res = await localRepository.getUnsyncedItems();
+
+    if (res.isEmpty) return;
+
+    if (localRepository.database == null) {
+      await localRepository.open();
+    }
+    
+    for (int i = 0; i < res.length; i++) {
+      res[i].isSync = true;
+
+      await remoteRepository.addData(res[i]);
+      await localRepository.update(res[i]);
+    }
+
+    final List<Item> responseSyncedData = await getLocalItems();
+
+    emit(FetchSyncDataSuccess(items: responseSyncedData));
+  }
+
+  Future<void> deleteAllLocalDB() async {
+    await localRepository.clearAllItems();
   }
 }
--- lib/core/module/home/application/service/cubit/home_state.dart
+++ lib/core/module/home/application/service/cubit/home_state.dart
@@ -1,9 +1,37 @@
 part of 'home_cubit.dart';
 
-class HomeState {
-  HomeState({
-    required this.data,
+class HomeState {}
+
+class FetchItemsLoading extends HomeState {}
+
+class FetchItemsSuccess extends HomeState {
+  FetchItemsSuccess({
+    required this.items,
   });
 
-  final QuoteStateDTO data;
+  final List<Item> items;
 }
+
+class FetchItemsFailed extends HomeState {}
+
+class AddItemsLoading extends HomeState {}
+
+class AddItemsSuccess extends HomeState {
+  AddItemsSuccess({
+    required this.item,
+  });
+
+  final Item item;
+}
+
+class AddItemsFailed extends HomeState {}
+
+class FetchSyncDataSuccess extends HomeState {
+  FetchSyncDataSuccess({
+    required this.items,
+  });
+
+  final List<Item> items;
+}
+
+class FetchSyncDataFailed extends HomeState {}
