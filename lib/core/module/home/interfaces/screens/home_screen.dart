b4407ea039c9e9c96d0be7ca6231df624a7250be
josephgcedeno <josephgcedeno@gmail.com>
2024-11-08 11:51:07 +0800

    feat: initialize sample screen

--- lib/core/module/home/interfaces/screens/home_screen.dart
+++ lib/core/module/home/interfaces/screens/home_screen.dart
@@ -1,38 +1,183 @@
-import 'package:flirt/core/module/home/interfaces/widgets/quotes_card.dart';
-import 'package:flirt/gen/assets.gen.dart';
+import 'package:flirt/core/infrastructures/repository/local_repository.dart';
+import 'package:flirt/core/infrastructures/repository/remote_repository.dart';
+import 'package:flirt/core/module/home/application/service/cubit/home_cubit.dart';
+import 'package:flirt/internal/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final double width = MediaQuery.of(context).size.width;
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Assets.images.nuxifyLogo.image(width: width * 0.3),
-                  const QuotesCard(),
-                ],
+class _HomeScreenState extends State<HomeScreen> {
+  final List<Item> data = <Item>[];
+
+  bool isLoading = true;
+  void alertDialogModal(List<Item> items, String title) => showDialog<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(title),
+            content: SizedBox(
+              height: 300,
+              width: 400,
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: items.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(
+                      'Item Name: ${items[index].name} \nSynced: ${items[index].isSync}',
+                    ),
+                  );
+                },
               ),
             ),
-            Container(
-              width: width,
-              padding: const EdgeInsets.all(20),
-              child: const Text(
-                'v1.3.0',
-                style: TextStyle(fontSize: 11, color: Colors.white54),
-                textAlign: TextAlign.center,
+          );
+        },
+      );
+
+  @override
+  void initState() {
+    super.initState();
+
+    context.read<HomeCubit>().getAllData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<HomeCubit, HomeState>(
+      listenWhen: (HomeState previous, HomeState current) =>
+          current is FetchSyncDataFailed ||
+          current is FetchSyncDataSuccess ||
+          current is AddItemsSuccess ||
+          current is FetchItemsLoading ||
+          current is FetchItemsSuccess,
+      listener: (BuildContext context, HomeState state) {
+        if (state is FetchItemsSuccess) {
+          setState(() {
+            data.addAll(state.items);
+            isLoading = false;
+          });
+        } else if (state is FetchItemsLoading) {
+          setState(() {
+            isLoading = true;
+          });
+        } else if (state is AddItemsSuccess) {
+          setState(() {
+            data.add(state.item);
+          });
+        } else if (state is FetchSyncDataSuccess) {
+          setState(() {
+            data.clear();
+            data.addAll(state.items);
+          });
+
+          showSnackbar(
+            context,
+            isSuccessful: true,
+            message: 'Successfully Synced',
+          );
+        } else if (state is FetchSyncDataFailed) {
+          showSnackbar(
+            context,
+            isSuccessful: false,
+            message: 'Cannot be synced, bacause no internet connection.',
+          );
+        }
+      },
+      child: Scaffold(
+        backgroundColor: Colors.grey[900],
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Colors.black38,
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Wrap(
+                      spacing: 10,
+                      children: <Widget>[
+                        SizedBox(
+                          width: 300,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              context.read<HomeCubit>().addData(
+                                    Item(
+                                      id: data.length,
+                                      name: 'Item ${data.length}',
+                                      value: data.length,
+                                    ),
+                                  );
+                            },
+                            child: const Text('Save'),
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            alertDialogModal(
+                              RemoteRepository.listOfData,
+                              'Remote Data',
+                            );
+                          },
+                          child: const Text('Get remote data'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () async {
+                            final List<Item> res =
+                                await context.read<HomeCubit>().getLocalItems();
+
+                            alertDialogModal(
+                              res,
+                              'Local Data',
+                            );
+                          },
+                          child: const Text('Get local data'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () async {
+                            context.read<HomeCubit>().syncDataWhenOnline();
+                          },
+                          child: const Text('Sync'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () async {
+                            context.read<HomeCubit>().deleteAllLocalDB();
+                          },
+                          child: const Text('Clear DB'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+              const Text(
+                'Items From Source',
+                style: TextStyle(color: Colors.white),
+              ),
+              Expanded(
+                child: isLoading
+                    ? const Center(child: CircularProgressIndicator())
+                    : ListView.builder(
+                        itemCount: data.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return ListTile(
+                            title: Text(
+                              data[index].name,
+                              style: const TextStyle(color: Colors.white),
+                            ),
+                          );
+                        },
+                      ),
+              ),
+            ],
+          ),
         ),
       ),
     );
