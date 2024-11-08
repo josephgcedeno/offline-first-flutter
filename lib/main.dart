4838b8f811cab04c2587bbcdafafd285fca90aee
josephgcedeno <josephgcedeno@gmail.com>
2024-11-08 11:52:27 +0800

    chore: add connectivity listener

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flirt/configs/themes.dart';
 import 'package:flirt/core/application/service/cubit/quote_api_cubit.dart';
+import 'package:flirt/core/infrastructures/repository/local_repository.dart';
 import 'package:flirt/core/infrastructures/repository/quote_repository.dart';
+import 'package:flirt/core/infrastructures/repository/remote_repository.dart';
 import 'package:flirt/core/module/home/application/service/cubit/home_cubit.dart';
 import 'package:flirt/core/module/home/interfaces/screens/home_screen.dart';
+import 'package:flirt/internal/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -19,6 +25,8 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final LocalRepository localRepository = LocalRepository();
+    final RemoteRepository remoteRepository = RemoteRepository();
     return MultiBlocProvider(
       providers: <BlocProvider<dynamic>>[
         BlocProvider<QuoteAPICubit>(
@@ -27,7 +35,10 @@ class App extends StatelessWidget {
           ),
         ),
         BlocProvider<HomeCubit>(
-          create: (BuildContext context) => HomeCubit(),
+          create: (BuildContext context) => HomeCubit(
+            localRepository: localRepository,
+            remoteRepository: remoteRepository,
+          ),
         ),
       ],
       child: MaterialApp(
@@ -42,7 +53,53 @@ class App extends StatelessWidget {
   }
 }
 
-class _HomePageState extends StatelessWidget {
+class _HomePageState extends StatefulWidget {
+  @override
+  State<_HomePageState> createState() => _HomePageStateState();
+}
+
+class _HomePageStateState extends State<_HomePageState> {
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<List<ConnectivityResult>> _connectivitySubscription;
+  int counter = 0;
+
+  void startMonitoringConnectivity() {
+    _connectivitySubscription = _connectivity.onConnectivityChanged
+        .listen((List<ConnectivityResult> result) {
+      /// TODO: Improve solution for checks initial unnecessary emits
+      if (counter <= 1) {
+        counter++;
+        return;
+      }
+      final bool isConnected = result.last != ConnectivityResult.none;
+
+      if (!mounted) return;
+      showSnackbar(
+        context,
+        isSuccessful: isConnected,
+        message: isConnected ? 'Syncing' : 'Offline',
+      );
+
+      if (result.first != ConnectivityResult.none) {
+        if (!mounted) return;
+
+        context.read<HomeCubit>().syncDataWhenOnline();
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startMonitoringConnectivity();
+  }
+
+  @override
+  void dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const HomeScreen();
