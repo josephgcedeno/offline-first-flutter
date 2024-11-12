4e41594cbef8a75a7b2285411f4221f07b183fdf
josephgcedeno <josephgcedeno@gmail.com>
2024-11-12 17:53:12 +0800

    chore: update database manager

--- lib/core/infrastructures/caching/database.dart
+++ lib/core/infrastructures/caching/database.dart
@@ -10,7 +10,7 @@ class DatabaseManager {
   Future<Database> get instance async => _database ??= await initializeDb();
 
   Future<Database> initializeDb() async => _database = await openDatabase(
-        join(await getDatabasesPath(), 'SampleApp.db'),
+        join(await getDatabasesPath(), 'SampleApp2.db'),
         version: 1,
         onCreate: (Database db, int version) async {
           await db.execute(
@@ -19,6 +19,10 @@ class DatabaseManager {
 
           await db.execute(
             'CREATE TABLE $catsTable(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name TEXT, createdAt INTEGER, synced INTEGER)',
+          );
+
+          await db.execute(
+            'CREATE TABLE $employeesTable(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name TEXT, createdAt INTEGER, synced INTEGER, EMPLOYEE_ID INTEGER, FIRST_NAME TEXT, LAST_NAME TEXT, EMAIL TEXT, PHONE_NUMBER TEXT, HIRE_DATE TEXT, JOB_ID TEXT, SALARY REAL, COMMISSION_PCT REAL, MANAGER_ID INTEGER, action TEXT, createdDate INTEGER, localId TEXT, modifiedDate INTEGER, synced INTEGER)',
           );
         },
       );
---
+++ lib/core/infrastructures/caching/employee_cache.dart
@@ -0,0 +1,68 @@
+import 'package:flirt/core/domain/models/employee/employee_request.dart';
+import 'package:flirt/core/domain/models/employee/employee_response.dart';
+import 'package:flirt/core/infrastructures/caching/database.dart';
+import 'package:flirt/core/infrastructures/caching/database_constant.dart';
+import 'package:sqflite/sqflite.dart';
+
+class EmployeeCache {
+  final DatabaseManager databaseManager = DatabaseManager();
+
+  Future<List<EmployeeResponse>> getItems() async => await _getLocalItems();
+
+  Future<void> saveToLocal(List<EmployeeResponse> response) async {
+    for (int i = 0; i < response.length; i++) {
+      await _insert(response[i]);
+    }
+  }
+
+  Future<List<EmployeeResponse>> _getLocalItems() async {
+    final Database dbInstance = await databaseManager.instance;
+
+    final List<Map<String, dynamic>> maps =
+        await dbInstance.query(employeesTable);
+
+    return List<EmployeeResponse>.generate(
+      maps.length,
+      (int i) => EmployeeResponse.fromJson(maps[i]),
+    );
+  }
+
+  Future<void> insertSingleItem(EmployeeRequest item) async {
+    final Database dbInstance = await databaseManager.instance;
+
+    await dbInstance.insert(
+      employeesTable,
+      item.toJsonInsertRecord(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<void> updateItem(EmployeeRequest item) async {
+    final Database dbInstance = await databaseManager.instance;
+
+    await dbInstance.update(
+      employeesTable,
+      item.toJsonInsertRecord(),
+      where: 'EMPLOYEE_ID = ?',
+      whereArgs: <int>[item.employeeId],
+    );
+  }
+
+  Future<int> _insert(EmployeeResponse item) async {
+    final Database dbInstance = await databaseManager.instance;
+
+    return await dbInstance.insert(
+      employeesTable,
+      item.toJsonInsertRecord(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<void> truncateRecord() async {
+    final Database dbInstance = await databaseManager.instance;
+
+    dbInstance.batch()
+      ..delete(employeesTable)
+      ..commit(noResult: true);
+  }
+}
