482a344bad43cd7ca0d2d6850f828cfa1e898b7d
josephgcedeno <josephgcedeno@gmail.com>
2024-11-12 18:10:31 +0800

    chore: update table

--- lib/core/infrastructures/caching/database.dart
+++ lib/core/infrastructures/caching/database.dart
@@ -10,7 +10,7 @@ class DatabaseManager {
   Future<Database> get instance async => _database ??= await initializeDb();
 
   Future<Database> initializeDb() async => _database = await openDatabase(
-        join(await getDatabasesPath(), 'SampleApp2.db'),
+        join(await getDatabasesPath(), 'SampleApp3.db'),
         version: 1,
         onCreate: (Database db, int version) async {
           await db.execute(
@@ -22,7 +22,7 @@ class DatabaseManager {
           );
 
           await db.execute(
-            'CREATE TABLE $employeesTable(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name TEXT, createdAt INTEGER, synced INTEGER, EMPLOYEE_ID INTEGER, FIRST_NAME TEXT, LAST_NAME TEXT, EMAIL TEXT, PHONE_NUMBER TEXT, HIRE_DATE TEXT, JOB_ID TEXT, SALARY REAL, COMMISSION_PCT REAL, MANAGER_ID INTEGER, action TEXT, createdDate INTEGER, localId TEXT, modifiedDate INTEGER, synced INTEGER)',
+            'CREATE TABLE $employeesTable(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, EMPLOYEE_ID INTEGER, FIRST_NAME TEXT, LAST_NAME TEXT, EMAIL TEXT, PHONE_NUMBER TEXT, HIRE_DATE TEXT, JOB_ID TEXT, SALARY REAL, COMMISSION_PCT REAL, MANAGER_ID INTEGER, action TEXT, createdDate INTEGER, localId TEXT, modifiedDate INTEGER, synced INTEGER)',
           );
         },
       );
